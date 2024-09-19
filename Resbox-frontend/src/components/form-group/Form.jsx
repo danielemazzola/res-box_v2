3b5f1278723ad8115b70ff57a160f23093bd81f7
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-09-19 12:33:55 +0200

    navigate dashboard after login

--- Resbox-frontend/src/components/form-group/Form.jsx
+++ Resbox-frontend/src/components/form-group/Form.jsx
@@ -69,6 +69,11 @@ const Form = ({ handleCloseModal, recovery = false }) => {
       token
     )
     if (response) {
+      if(formType.login){
+        setTimeout(() => {
+          navigate('./dashboard')
+        }, 1500);
+      }
       if (formType.register) {
         const loginFormType = { login: true, register: false }
         const loginResponse = await fetchSubmit(
