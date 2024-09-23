8dc008c1e2d9c07f61f7c2a9a9c3a34686104107
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-09-24 00:23:42 +0200

    FIX App

--- Resbox-frontend/src/App.jsx
+++ Resbox-frontend/src/App.jsx
@@ -19,7 +19,6 @@ const App = () => {
         <Route path={`/dashboard`} element={<ProtectedRoute><Dashboard /></ProtectedRoute>}/>
         <Route path={`/my-boxes`} element={<ProtectedRoute><MyBox /></ProtectedRoute> }/>
         <Route path={`/promo-box`} element={<ProtectedRoute><PromoBox /></ProtectedRoute>}/>
-        <Route path={`/partner`} element={<ProtectedRoute><Partner /></ProtectedRoute>}/>
         <Route path='*' element={<NotFound />} />
       </Route>
     </Routes>
