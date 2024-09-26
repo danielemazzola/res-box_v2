f018f9f9efeaf22a507fb28db70926a0a6833dc5
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-09-27 00:49:24 +0200

    FIX z index canvas

--- Resbox-frontend/src/helpers/canvas.js
+++ Resbox-frontend/src/helpers/canvas.js
@@ -1,7 +1,7 @@
 export const canvas_index = () => {
   const canvas = document.querySelector('canvas')
   if (canvas) {
-    canvas.style.zIndex = '9999'
+    canvas.style.zIndex = '1001'
     //canvas.style.position = 'fixed'
   }
 }
