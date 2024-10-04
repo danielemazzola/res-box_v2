eb41724fcd5c4a35f447367ca67f2c69f860cf89
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-10-04 11:25:44 +0200

    SHOW footer when isAuth is false

--- Resbox-frontend/src/components/footer/Footer.jsx
+++ Resbox-frontend/src/components/footer/Footer.jsx
@@ -5,39 +5,49 @@ const Footer = () => {
   const date = new Date()
   const year = date.getFullYear()
   return (
-    <footer>
-      <div className='footer__content-info'>
-        <div>
-          <h5>Colabora con Res-Box</h5>
-          <p>Res-Box para Partners</p>
-          <p>Cómo funsiona</p>
+    <>
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+        <path
+          fill='#ececec'
+          fillOpacity='1'
+          d='M0,160L80,176C160,192,320,224,480,240C640,256,800,256,960,261.3C1120,267,1280,277,1360,282.7L1440,288L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z'
+        ></path>
+      </svg>
+
+      <footer>
+        <div className='footer__content-info'>
+          <div>
+            <h5>Colabora con Res-Box</h5>
+            <p>Res-Box para Partners</p>
+            <p>Cómo funsiona</p>
+          </div>
+          <div>
+            <h5>Enlaces de interes</h5>
+            <p>Condiciones del servicio Partners</p>
+            <p>Politicas de Privacidad</p>
+            <p>Contacto</p>
+          </div>
+          <div>
+            <h5>Síguenos</h5>
+            <p>Facebook</p>
+            <p>Linkedin</p>
+            <p>Instagram</p>
+          </div>
         </div>
-        <div>
-          <h5>Enlaces de interes</h5>
-          <p>Condiciones del servicio Partners</p>
-          <p>Politicas de Privacidad</p>
-          <p>Contacto</p>
+        <div className='footer__content-reserved'>
+          <div className='coffee-drop'></div>
+          <div className='footer__content-reserved-info'>
+            <span>
+              All Rights Reserved &#174;​{' '}
+              {year === 2024 ? '2024' : `2024-${year}`}
+            </span>
+            <i className='text-gray-300 font-light'>Powered by</i>
+            <img alt='Logo Res-Box' src={logo} width='70' loading='lazy' />
+            <i className='text-gray-300 font-semibold'>RES-BOX</i>
+          </div>
         </div>
-        <div>
-          <h5>Síguenos</h5>
-          <p>Facebook</p>
-          <p>Linkedin</p>
-          <p>Instagram</p>
-        </div>
-      </div>
-      <div className='footer__content-reserved'>
-        <div class='coffee-drop'></div>
-        <div className='footer__content-reserved-info'>
-          <span>
-            All Rights Reserved &#174;​{' '}
-            {year === 2024 ? '2024' : `2024-${year}`}
-          </span>
-          <i className='text-gray-300 font-light'>Powered by</i>
-          <img alt='Logo Res-Box' src={logo} width='70' loading='lazy' />
-          <i className='text-gray-300 font-semibold'>RES-BOX</i>
-        </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   )
 }
 
--- Resbox-frontend/src/layout/Layout.jsx
+++ Resbox-frontend/src/layout/Layout.jsx
@@ -15,11 +15,13 @@ const Layout = () => {
     stateLoader: { load }
   } = useContext(ReducersContext)
   const { isAuth, token } = useContext(AuthContext)
+  const { stateIsAuth } = useContext(ReducersContext)
   useEffect(() => {
     if (token) {
       isAuth()
     }
   }, [])
+
   return (
     <>
       {load && <Loader />}
@@ -38,14 +40,7 @@ const Layout = () => {
         <ButtonTopScroll />
         <Outlet />
       </main>
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
-        <path
-          fill='#ececec'
-          fillOpacity='1'
-          d='M0,160L80,176C160,192,320,224,480,240C640,256,800,256,960,261.3C1120,267,1280,277,1360,282.7L1440,288L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z'
-        ></path>
-      </svg>
-      <Footer />
+      {!stateIsAuth.isAuth && <Footer />}
     </>
   )
 }
